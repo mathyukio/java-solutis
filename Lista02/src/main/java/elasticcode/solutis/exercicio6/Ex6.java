f04647a846f920ece581f5b5dba1b03bd15664d9
MATHEUS YUKIO MAKIYAMA <matheus.makiyama@solutis.net.br>
2025-04-17 09:12:42 -0300

    removendo a lista02

--- Lista02/src/main/java/elasticcode/solutis/exercicio6/Ex6.java
+++
@@ -1,7 +0,0 @@
-package elasticcode.solutis.exercicio6;
-
-public class Ex6 {
-    public static void main(String[] args) {
-        System.out.println("Área ocupada: %.2f metros". formatted(Math.PI * (25)));
-    }
-}
